e298e195d10dc5861c938b2c228442c750bf2d59
manuel17 <manuel17Co>
2025-10-25 16:02:25 -0500

    Modificacion metadata final

--- app/(webpage)/layout.tsx
+++ app/(webpage)/layout.tsx
@@ -2,31 +2,12 @@ import type { Metadata } from "next";
 import HeaderComponent from "@/components/organisms/HeaderComponent";
 
 export const metadata: Metadata = {
-  title: 'Mercado Libre | Inicio',
-  description: 'Descubre los mejores productos, ofertas y servicios en Mercado Libre. Compra fácil, rápido y seguro.',
-  keywords: ['mercado libre', 'compras', 'productos', 'ofertas', 'envío gratis', 'más vendidos'],
+  title: "Mercado Libre | Inicio",
+  description: "Descubre los mejores productos, ofertas y servicios en Mercado Libre.",
   openGraph: {
-    title: 'Mercado Libre | Inicio',
-    description: 'Descubre los mejores productos, ofertas y servicios en Mercado Libre. Compra fácil, rápido y seguro.',
-    url: 'https://www.mercadolibre.com/',
-    siteName: 'Mercado Libre',
-    images: [
-      {
-        url: 'https://http2.mlstatic.com/D_NQ_NP_2X_601857-MLA5495309220_042023-F.webp',
-        width: 800,
-        height: 600,
-        alt: 'Mercado Libre productos',
-      },
-    ],
-    locale: 'es_CO',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Mercado Libre | Inicio',
-    description: 'Descubre los mejores productos, ofertas y servicios en Mercado Libre. Compra fácil, rápido y seguro.',
-    images: ['https://http2.mlstatic.com/D_NQ_NP_2X_601857-MLA5495309220_042023-F.webp'],
-    site: '@mercadolibre',
+    title: "Mercado Libre | Inicio",
+    description: "Descubre los mejores productos, ofertas y servicios en Mercado Libre.",
+    images: ["https://http2.mlstatic.com/D_NQ_NP_2X_601857-MLA5495309220_042023-F.webp"],
   },
 };
 
@@ -38,7 +19,7 @@ export default function WebpageLayout({
   return (
     <main>
       <HeaderComponent />
-      { children }
+      {children}
     </main>
   );
 }
